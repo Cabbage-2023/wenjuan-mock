d1af49ce228650da8f37d6ac423f8050ba007a27
CabbageLee <13613409672@163.com>
2026-05-05 23:28:52 +0800

    问卷编辑器开发完成

--- mock/question.js
+++ mock/question.js
@@ -15,6 +15,10 @@ module.exports=[
         data:{
           id:Random.id(),
           title:Random.ctitle(),
+          desc:'问卷描述',
+          js:'',
+          css:'',
+          
           componentList:[
             //title
             {
